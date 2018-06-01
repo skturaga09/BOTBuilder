40686fb944b277f206851c0fc82aaa852a33923f
skturaga09 <39695657+skturaga09@users.noreply.github.com>
2018-06-01 15:02:20 +0530

    Initial Code

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=sanacnbot-Web-Deploy.pubxml -p:Password=D5QzcK6YJGofh7Tsr5EivWN368jhtF0Gs0YGhqEokk4uCnDEXCJpDTY0E9my
+
